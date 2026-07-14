f6b3df393725d2a716876b48f30cbfc35a75df66
SetsunaXpro <showarider812@gmail.com>
2026-07-14 09:18:27 +0700

    finalize aseli terakhir inima final revisi bismillah (1)(1) gajadi ini beneran final aminnn no revisi(2) sumpah ini last revisi final bismillah ya allah final banget(1)

--- routes/web.php
+++ routes/web.php
@@ -1,62 +1,41 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BikeController;
-use App\Models\Bike;
-use App\Http\Controllers\BookingController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\BikeController;
+use App\Http\Controllers\BookingController;
+use App\Http\Controllers\ProfileController;
+use App\Models\Bike;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    $bikes = Bike::take(3)->get();
+    return view('welcome', compact('bikes'));
+});
+
+Route::get('/bikes', [BikeController::class, 'index']);
+
+Route::get('/bike/{bike}', [BikeController::class, 'show'])
+    ->middleware('auth');
+
+Route::post('/bike/{bike}/book', [BookingController::class, 'store'])
+    ->middleware('auth')
+    ->name('booking.store');
 
 Route::get('/customer-dashboard', [BookingController::class, 'index'])
     ->middleware('auth')
     ->name('dashboard');
 
-Route::post('/bike/{bike}/book',
-    [BookingController::class, 'store'])
-    ->name('booking.store')
-    ->middleware('auth');
-
-Route::get('/bike/{bike}', [BikeController::class, 'show'])
-    ->middleware('auth');
-
-Route::get('/bike/{bike}', function (Bike $bike) {
-    return view('bike-detail', compact('bike'));
-});
-
-Route::get('/', function () {
-    $bikes = Bike::take(3)->get();
-
-    return view('welcome', compact('bikes'));
-});
-Route::get('/dashboard', function () {
-    return redirect('/');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::get('/admin', function () {
-
-    if (auth()->user()->role !== 'admin') {
-        abort(403);
-    }
-
-    return view('admin');
-
-})->middleware('auth');
-Route::get('/bike-detail', function () {
-    return view('bike-detail');
-});
-
-Route::patch(
-'/booking/{booking}/accept',
-[BookingController::class,'accept']
-)
-->middleware('auth')
-->name('booking.accept');
+Route::patch('/booking/{booking}/accept', [BookingController::class, 'accept'])
+    ->middleware('auth')
+    ->name('booking.accept');
 
 Route::get('/admin', [AdminController::class, 'index'])
-    ->middleware('auth');
+    ->middleware('auth')
+    ->name('admin');
 
-Route::get('/bikes', [BikeController::class, 'index']);
-
+Route::get('/dashboard', function () {
+    return redirect('/');
+})->middleware(['auth', 'verified']);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
